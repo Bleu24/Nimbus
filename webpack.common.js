56de3fd010b9f3d9b82315992d1976cb4d3a9fce
Bryan Luise Reamillo <137306067+Bleu24@users.noreply.github.com>
2026-03-12 01:02:07 +0800

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./src/main.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.bundle.js",
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: "/\.css$/i",
+                use: ["style-loader", "css-loader"]
+            }, 
+            {
+                test: "/\.(png|svg|gif|jpeg|jpg)$/i",
+                use: "asset/resource"
+            },
+            {
+                test: "/\.(woff2|woff|otf|ttf|eot)$/i",
+                use: "asset/resource"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+            title: "webpack-template" // change this, this chanes the title docs
+        })
+    ]
+}
