508ccef9edc234bd7f74e8bf588e39d7e1ccaa51
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-03-12 10:31:50 +0800

    Refactor webpack.dev.js to use ES module syntax

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,11 @@
-const common = require('./webpack.common.js');
-const { merge } = require('webpack-merge');
+import common from './webpack.common.js';
+import { merge } from 'webpack-merge';
 
-module.exports = merge(common, {
+export default merge(common, {
     mode: 'development',
     devtool: 'eval-source-map',
     devServer: {
-        port: 5500 // default 8080
+        port: 5500, // default 8080
         watchFiles: ['./src/main.js'],
         open: true
     },
