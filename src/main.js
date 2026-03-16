a054d8991d1542934db8197e0c55778ea91bfcbd
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-03-17 03:22:35 +0800

    feat: initialize WeatherService in main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import "./styles.css";
 import "./ui/home.css";
 
 import { Home } from "./ui/home.js";
+import { WeatherService } from "./services/WeatherService.js";
 
 
 document.body.appendChild(Home);
+
+WeatherService.initialize();
